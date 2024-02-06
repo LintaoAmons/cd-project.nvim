3099a7b6814d6d5d8f625f73d01ebbae1ba14812
vague2k <ilovedrawing056@gmail.com>
2024-02-06 13:32:00 -0500

    add: manual project add, uses `vim.ui.input()`

--- lua/cd-project/adapter/vim-ui.lua
+++ lua/cd-project/adapter/vim-ui.lua
@@ -4,39 +4,43 @@ local repo = require("cd-project.project-repo")
 
 ---@param opts? table
 local function cd_project(opts)
-	opts = opts or {}
-	local projects = repo.get_projects()
+    opts = opts or {}
+    local projects = repo.get_projects()
 
-	local maxLength = 0
-	for _, project in ipairs(projects) do
-		if #project.name > maxLength then
-			maxLength = #project.name
-		end
-	end
+    local maxLength = 0
+    for _, project in ipairs(projects) do
+        if #project.name > maxLength then
+            maxLength = #project.name
+        end
+    end
 
-	vim.ui.select(projects, {
-		prompt = "Select a directory",
-		format_item = function(project)
-			return utils.format_entry(project, maxLength)
-		end,
-	}, function(selected)
-		if not selected then
-			return utils.log_error("Must select a valid dir")
-		end
-		api.cd_project(selected.path)
-	end)
+    vim.ui.select(projects, {
+        prompt = "Select a directory",
+        format_item = function(project)
+            return utils.format_entry(project, maxLength)
+        end,
+    }, function(selected)
+        if not selected then
+            return utils.log_error("Must select a valid dir")
+        end
+        api.cd_project(selected.path)
+    end)
 end
 
 local function manual_cd_project()
-	-- ISSUE: vague2k: was getting weird behavior using a nested vim.ui.input() callback
-	-- and could not find appropriate docs or resources to help
-	-- ui is very bare bones but it works as intended
-	local final_path = vim.fn.input("Enter path: ")
-	local final_name = vim.fn.input("Enter name (optional): ")
-	api.add_project(final_path, final_name)
+    -- TODO: needs error handling, but works as intended
+    vim.ui.input({ prompt = "Add a project path: " }, function(path)
+        local final_path
+        final_path = path
+        vim.ui.input({ prompt = "Add a project name: " }, function(name)
+            local final_name
+            final_name = name
+            api.add_project(final_path, final_name)
+        end)
+    end)
 end
 
 return {
-	cd_project = cd_project,
-	manual_cd_project = manual_cd_project,
+    cd_project = cd_project,
+    manual_cd_project = manual_cd_project,
 }
