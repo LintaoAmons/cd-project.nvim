e2cc03d3663c503b4ff9bf46252962d68a9200c9
Albert Hernandez <ilovedrawing056@gmail.com>
2024-02-07 03:40:01 -0500

    add: error handling in vim-ui adapter

--- lua/cd-project/adapter/vim-ui.lua
+++ lua/cd-project/adapter/vim-ui.lua
@@ -30,11 +30,29 @@ end
 local function manual_cd_project()
 	-- TODO: needs error handling, but works as intended
 	vim.ui.input({ prompt = "Add a project path: " }, function(path)
+		if not path or path == "" then
+			utils.log_error("No path given, quitting.")
+			return
+		end
+
 		local final_path
 		final_path = path
+
 		vim.ui.input({ prompt = "Add a project name: " }, function(name)
+			if not name then
+				utils.log_error("Quit command from name input, dir was not added")
+				return
+			end
+
+			if name == "" then
+				vim.notify('No name given, using "' .. utils.get_tail_of_path(final_path) .. '" instead')
+				api.add_project(final_path)
+				return
+			end
+
 			local final_name
 			final_name = name
+
 			api.add_project(final_path, final_name)
 		end)
 	end)
