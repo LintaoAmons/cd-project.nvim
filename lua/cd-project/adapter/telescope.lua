c45061c63adfb5b66664e7d7a152a623c3c3e37b
vague2k <ilovedrawing056@gmail.com>
2024-02-09 22:48:27 -0500

    refactor: use `format_dir_based_on_os` in telescope adapter

--- lua/cd-project/adapter/telescope.lua
+++ lua/cd-project/adapter/telescope.lua
@@ -9,7 +9,6 @@ local finders = require("telescope.finders")
 local conf = require("telescope.config").values
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
-local action_set = require("telescope.actions.set")
 
 local repo = require("cd-project.project-repo")
 local api = require("cd-project.api")
@@ -27,7 +26,7 @@ local cd_project = function(opts)
 
 	pickers
 		.new(opts, {
-			attach_mappings = function(prompt_bufnr, map)
+			attach_mappings = function(prompt_bufnr)
 				actions.select_default:replace(function()
 					actions.close(prompt_bufnr)
 					---@type CdProject.Project
@@ -56,12 +55,6 @@ end
 local manual_cd_project = function(opts)
 	opts = opts or {}
 
-	-- TODO: test on windows, most likely does not because windows is dookie and wants
-	-- to be different SOOO bad
-	--
-	-- most likely will need to handle how cwd property in jobstart is handled for windows... %USERPROFILE% ???
-
-	-- check if the find or fd command exists on the users system
 	local find_command = utils.check_for_find_cmd()
 	if not find_command then
 		utils.log_error("You need to install fd or find.")
@@ -71,6 +64,7 @@ local manual_cd_project = function(opts)
 	-- Harding coding the Home directory as a starting point for finding
 	-- directories is desireable since it's very unlikely a user will have
 	-- a project worth adding to the cd list in any parent dir relative to the home dir.
+	-- On Windows: ~ == "<drive>:/Users/<user>"
 	vim.fn.jobstart(find_command, {
 		cwd = vim.fn.expand("~"),
 		stdout_buffered = true,
@@ -81,27 +75,27 @@ local manual_cd_project = function(opts)
 						actions.select_default:replace(function()
 							actions.close(prompt_bufnr)
 							local selected_dir = action_state.get_selected_entry().value
-							local formatted_path = vim.fn.expand("~") .. "/" .. selected_dir
+							local formatted_path = utils.format_dir_based_on_os(selected_dir)
 
-							-- TODO: using vim.ui for name input feels hacky...
-							-- if there's a way to use telescope instead of vim.ui for this, that would be preferred
 							vim.ui.input({ prompt = "Add a project name: " }, function(name)
-								if not name then
-									utils.log_error("Quit command from name input, dir was not added")
-									return
-								end
-
-								if name == "" then
+								if not name or name == "" then
 									vim.notify(
 										'No name given, using "'
 											.. utils.get_tail_of_path(formatted_path)
 											.. '" instead'
 									)
-									api.add_project(formatted_path)
-									return
+									local project = api.build_project_obj(formatted_path)
+									if not project then
+										return
+									end
+									return api.add_project(project)
 								end
 
-								api.add_project(formatted_path, name)
+								local project = api.build_project_obj(formatted_path, name)
+								if not project then
+									return
+								end
+								return api.add_project(project)
 							end)
 						end)
 						return true
